5e1869921f3dcadf336d7a69ccb5d71a73429216
unknown <francico.l.teixeira@aluno.senai.br>
2026-03-10 10:48:08 -0500

    aaaaaa

---
+++ script.js
@@ -0,0 +1,86 @@
+const lista = document. querySelector(".lista-produtos")
+
+const cafe = [
+
+{
+    nome:"espresso"
+    descricao:"cafe concentrado y aromatico"
+    preco:"$3.50"
+   img:"assents/produtos/produtos01.png"
+},
+
+{
+  nome:"cappucino",
+  descricao:"espresso con leche vaporizada y espuma",
+  preco:"$4.50",
+  img:""
+},
+
+
+{
+  nome:"cafe helando",
+  descricao:"refrescante cafe frio con hielio",
+  preco:"$4.50",
+  img:""
+}
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+]
